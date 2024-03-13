0aaf276eb5e357a51df543796b93ece26211ea29
Akatsuki Levi <akatsukilevi@yahoo.co.jp>
2024-03-13 17:46:44 -0300

    feat: Reduced clutter on game list

--- src/yuzu/game_list.cpp
+++ src/yuzu/game_list.cpp
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2015 Citra Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include <regex>
 #include <QApplication>
 #include <QDir>
 #include <QFileInfo>
@@ -411,6 +410,10 @@ void GameList::AddEntry(const QList<QStandardItem*>& entry_items, GameListDir* p
     parent->appendRow(entry_items);
 }
 
+void GameList::AddRootEntry(const QList<QStandardItem*>& entry_items) {
+    item_model->invisibleRootItem()->appendRow(entry_items);
+}
+
 void GameList::ValidateEntry(const QModelIndex& item) {
     const auto selected = item.sibling(item.row(), 0);
 
@@ -466,7 +469,7 @@ bool GameList::IsEmpty() const {
 void GameList::DonePopulating(const QStringList& watch_list) {
     emit ShowList(!IsEmpty());
 
-    item_model->invisibleRootItem()->appendRow(new GameListAddDir());
+    // item_model->invisibleRootItem()->appendRow(new GameListAddDir());
 
     // Add favorites row
     item_model->invisibleRootItem()->insertRow(0, new GameListFavorites());
@@ -894,22 +897,21 @@ void GameList::AddFavorite(u64 program_id) {
     auto* favorites_row = item_model->item(0);
 
     for (int i = 1; i < item_model->rowCount() - 1; i++) {
-        const auto* folder = item_model->item(i);
-        for (int j = 0; j < folder->rowCount(); j++) {
-            if (folder->child(j)->data(GameListItemPath::ProgramIdRole).toULongLong() ==
-                program_id) {
-                QList<QStandardItem*> list;
-                for (int k = 0; k < COLUMN_COUNT; k++) {
-                    list.append(folder->child(j, k)->clone());
-                }
-                list[0]->setData(folder->child(j)->data(GameListItem::SortRole),
-                                 GameListItem::SortRole);
-                list[0]->setText(folder->child(j)->data(Qt::DisplayRole).toString());
-
-                favorites_row->appendRow(list);
-                return;
-            }
+        const auto* game = item_model->item(i);
+        if (game->data(GameListItemPath::ProgramIdRole).toULongLong() != program_id) {
+            continue;
         }
+
+        QList<QStandardItem*> list;
+        for (int j = 0; j < COLUMN_COUNT; j++) {
+            list.append(item_model->item(i, j)->clone());
+        }
+
+        list[0]->setData(game->data(GameListItem::SortRole), GameListItem::SortRole);
+        list[0]->setText(game->data(Qt::DisplayRole).toString());
+
+        favorites_row->appendRow(list);
+        return;
     }
 }
 
