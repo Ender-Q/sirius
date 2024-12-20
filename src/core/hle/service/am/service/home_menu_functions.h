suyu-emu/suyu-v0.0.4

Implemented some basic sleep functions

src/core/hle/service/am/service/home_menu_functions.h
@@ -24,6 +24,7 @@ private:
     Result LockForeground();
     Result UnlockForeground();
     Result GetPopFromGeneralChannelEvent(OutCopyHandle<Kernel::KReadableEvent> out_event);
+    Result IsSleepEnabled(Out<bool> out_is_sleep_enbaled);
     Result IsRebootEnabled(Out<bool> out_is_reboot_enbaled);
     Result IsForceTerminateApplicationDisabledForDebug(
         Out<bool> out_is_force_terminate_application_disabled_for_debug);

Ender-Q/sirius

src/core/hle/service/am/service/home_menu_functions.h
@@ -24,7 +24,8 @@ private:
     Result LockForeground();
     Result UnlockForeground();
     Result GetPopFromGeneralChannelEvent(OutCopyHandle<Kernel::KReadableEvent> out_event);
-    Result IsRebootEnabled(Out<bool> out_is_reboot_enbaled);
+    Result IsSleepEnabled(Out<bool> out_is_sleep_enabled);
+    Result IsRebootEnabled(Out<bool> out_is_reboot_enabled);
     Result IsForceTerminateApplicationDisabledForDebug(
         Out<bool> out_is_force_terminate_application_disabled_for_debug);
 