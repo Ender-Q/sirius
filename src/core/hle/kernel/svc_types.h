pflyly/citron-mirror

kernel: Implement SystemResourceSize info type for VAMM initialization
Adds support for InfoType::SystemResourceSize (0x1C) which is required for
proper initialization of the Virtual Address Memory Manager (VAMM). This
implementation:

1. Adds SystemResourceSize to the InfoType enum in svc_types.h
2. Implements the GetInfo handler for SystemResourceSize in svc_info.cpp
3. Returns 512MB (0x20000000 bytes) as the system resource size
4. Adds debug logging for the SVC call

The 512MB value is chosen based on typical system resource allocations
needed for VAMM initialization on the Nintendo Switch.

This fixes crashes in games that rely on VAMM functionality, particularly
during nn::os::detail::VammManager::InitializeIfEnabled().

src/core/hle/kernel/svc_types.h
@@ -156,6 +156,7 @@ enum class InfoType : u32 {
 
     MesosphereMeta = 65000,
     MesosphereCurrentProcess = 65001,
+    SystemResourceSize = 0x1C,
 };
 
 enum class BreakReason : u32 {

Ender-Q/sirius

src/core/hle/kernel/svc_types.h
@@ -157,6 +157,7 @@ enum class InfoType : u32 {
 
     MesosphereMeta = 65000,
     MesosphereCurrentProcess = 65001,
+    SystemResourceSize = 0x1D,
 };
 
 enum class BreakReason : u32 {