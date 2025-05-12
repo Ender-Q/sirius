pflyly/citron-mirror

Fix a few strings

src/android/app/src/main/java/org/citron/citron_emu/network/NetPlayManager.kt
@@ -42,7 +42,7 @@ object NetPlayManager {
     }
 
     fun getUsername(activity: Context): String {        val prefs = PreferenceManager.getDefaultSharedPreferences(activity)
-        val name = "Lime3ds${(Math.random() * 100).toInt()}"
+        val name = "Citron${(Math.random() * 100).toInt()}"
         return prefs.getString("NetPlayUsername", name) ?: name
     }
 

Ender-Q/sirius

src/android/app/src/main/java/org/yuzu/yuzu_emu/network/NetPlayManager.kt
@@ -42,7 +42,7 @@ object NetPlayManager {
     }
 
     fun getUsername(activity: Context): String {        val prefs = PreferenceManager.getDefaultSharedPreferences(activity)
-        val name = "Lime3ds${(Math.random() * 100).toInt()}"
+        val name = "Yuzu${(Math.random() * 100).toInt()}"
         return prefs.getString("NetPlayUsername", name) ?: name
     }
 